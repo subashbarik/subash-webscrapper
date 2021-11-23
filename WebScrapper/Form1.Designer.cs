e8db240ac8012fd6e7afebf728fca7f755ca012a
subashlaptop <codesubash79gmail.com>
2021-11-23 16:07:44 +0530

    Initial Commit

---
+++ WebScrapper/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WebScrapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnGetHTML = new System.Windows.Forms.Button();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(32, 150);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1070, 332);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnGetHTML
+            // 
+            this.btnGetHTML.Location = new System.Drawing.Point(948, 102);
+            this.btnGetHTML.Name = "btnGetHTML";
+            this.btnGetHTML.Size = new System.Drawing.Size(154, 23);
+            this.btnGetHTML.TabIndex = 1;
+            this.btnGetHTML.Text = "Get HTML";
+            this.btnGetHTML.UseVisualStyleBackColor = true;
+            this.btnGetHTML.Click += new System.EventHandler(this.btnGetHTML_Click);
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(81, 104);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(849, 20);
+            this.txtURL.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "URL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1149, 538);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.btnGetHTML);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnGetHTML;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WebScrapper/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Xml;
+
+namespace WebScrapper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private async void getHTMLAsync()
+        {
+            //string url = "https://www.flipkart.com/mobiles/~iphone-8-plus-64gb-and-256gb/pr?sid=tyy%2C4io&otracker=clp_banner_1_7.bannerX3.BANNER_apple-products-store_W8OZI07LNL&fm=neo%2Fmerchandising&iid=M_2763664d-4784-45ab-ac94-877d156bb162_7.W8OZI07LNL&ppt=clp&ppn=apple-products-store&ssid=r2ity58tww0000001571980915464";
+            //string url = "https://www.amazon.in/b/ref=cepc_elec_JupW3_rec1_lap?node=17492328031&pf_rd_p=d00fc980-f015-428c-a507-7773445b1343&pf_rd_r=KB5HJF69Z13A57AF8CGX";
+
+            string url = "https://www.ebay.com/b/iPhone-7-Plus/9355/bn_36834266";
+            string products = "";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var htmlDoc = new HtmlAgilityPack.HtmlDocument();
+            htmlDoc.LoadHtml(html);
+            var productHTML = htmlDoc.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("s-item__info clearfix"));
+            foreach (var productDetail in productHTML)
+            {
+                var productName = productDetail.Descendants("a").Where(node => node.GetAttributeValue("class", "").Equals("s-item__link"));
+                //productName.de
+                //products = products + productName.ToString() + "\n";
+            }
+            //richTextBox1.Text = products;
+        }
+
+        private void btnGetHTML_Click(object sender, EventArgs e)
+        {
+            getHTMLAsync();
+        }
+    }
+}
